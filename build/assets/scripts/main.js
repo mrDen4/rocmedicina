637d97e6bf78e7a1ba7581af56f8e105a60e04e3
Dennis <mr.den.saint@yandex.ru>
2019-05-13 12:46:09 +0300

    add popup

--- build/assets/scripts/main.js
+++ build/assets/scripts/main.js
@@ -8,8 +8,15 @@ $(document).ready(function () {
     
     $('.bg').on('click', function () {
         $('.header__menu-list').removeClass('header__menu-list--active');
-        $('.menu__img--exit').toggleClass('menu__img--exit--active');
-        $('.menu__img--open').toggleClass('menu__img--open--active');
+        $('.menu__img--exit').removeClass('menu__img--exit--active');
+        $('.menu__img--open').removeClass('menu__img--open--active');
+        $('.popup-buy').removeClass('popup-buy--active');
         $('.bg').removeClass('bg--active');
+        $('.bg').removeClass('bg--active-popup');
+    });
+
+    $('.hero .btn').on('click', function () {
+        $('.popup-buy').toggleClass('popup-buy--active');
+        $('.bg').toggleClass('bg--active-popup');
     });
 });
